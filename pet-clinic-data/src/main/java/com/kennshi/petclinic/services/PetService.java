d0861f6932c71bbf24d4589ce1e05fa6e49d64c6
eduard <edwardbs876@gmail.com>
2022-11-02 19:14:19 +0200

    Implement Interfaces for Services. closes #5

---
+++ pet-clinic-data/src/main/java/com/kennshi/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.kennshi.petclinic.services;
+
+import com.kennshi.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet owner);
+
+    Set<Pet> findAll();
+}
