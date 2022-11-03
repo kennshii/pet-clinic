4e103f6d1d095e639b302084f46ea68a4983580b
eduard <edwardbs876@gmail.com>
2022-11-03 21:32:45 +0200

    refactoring to common crud interface. closes #10

--- pet-clinic-data/src/main/java/com/kennshi/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/kennshi/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.kennshi.petclinic.services;
 
 import com.kennshi.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/kennshi/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/kennshi/petclinic/services/VetService.java
@@ -2,12 +2,6 @@ package com.kennshi.petclinic.services;
 
 import com.kennshi.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
