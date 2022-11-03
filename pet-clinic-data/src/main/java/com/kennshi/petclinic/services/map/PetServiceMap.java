8543988eaeabe213ee19d9c8217fe8996c92656b
eduard <edwardbs876@gmail.com>
2022-11-03 22:00:24 +0200

    Implementing Map based services. closes #11

--- pet-clinic-data/src/main/java/com/kennshi/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/kennshi/petclinic/services/map/PetServiceMap.java
@@ -1,2 +1,33 @@
-package com.kennshi.petclinic.services.map;public class PetServiceMap {
+package com.kennshi.petclinic.services.map;
+
+import com.kennshi.petclinic.model.Pet;
+import com.kennshi.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
 }
