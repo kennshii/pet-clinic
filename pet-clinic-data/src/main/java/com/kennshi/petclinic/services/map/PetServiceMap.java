f5911a2c4c5f11ec1376b8947232869151979f95
Eduard <edwardbs876@gmail.com>
2022-11-07 21:28:04 +0200

    adding bootstrap for data. closes #14

--- pet-clinic-data/src/main/java/com/kennshi/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/kennshi/petclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.kennshi.petclinic.services.map;
 
 import com.kennshi.petclinic.model.Pet;
-import com.kennshi.petclinic.services.CrudService;
+import com.kennshi.petclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
