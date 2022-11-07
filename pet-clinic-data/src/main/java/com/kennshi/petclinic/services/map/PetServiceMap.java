223cf56c3cebe70767110098cd21c933d10ced48
Eduard <edwardbs876@gmail.com>
2022-11-07 22:25:33 +0200

    added spring config. closes #22

--- pet-clinic-data/src/main/java/com/kennshi/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/kennshi/petclinic/services/map/PetServiceMap.java
@@ -2,9 +2,11 @@ package com.kennshi.petclinic.services.map;
 
 import com.kennshi.petclinic.model.Pet;
 import com.kennshi.petclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
--- pet-clinic-data/src/main/java/com/kennshi/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/kennshi/petclinic/services/map/VetServiceMap.java
@@ -2,9 +2,11 @@ package com.kennshi.petclinic.services.map;
 
 import com.kennshi.petclinic.model.Vet;
 import com.kennshi.petclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
