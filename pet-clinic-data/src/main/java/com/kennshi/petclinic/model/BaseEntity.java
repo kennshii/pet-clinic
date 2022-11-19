f2c35b33be65037dd1d880069b3b2f94132b8cee
Eduard <edwardbs876@gmail.com>
2022-11-19 18:59:19 +0200

    Updated base Entity with JPA annotations. Closes #34

--- pet-clinic-data/src/main/java/com/kennshi/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/kennshi/petclinic/model/BaseEntity.java
@@ -1,8 +1,18 @@
 package com.kennshi.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     public Long getId() {
         return id;
     }
@@ -10,8 +20,6 @@ public class BaseEntity implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
-
-    private Long id;
 
 
 }
