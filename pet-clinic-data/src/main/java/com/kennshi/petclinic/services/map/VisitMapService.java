62ea4fbb5cf1bd9aaa463783fa1c572a726af8bb
Eduard <edwardbs876@gmail.com>
2022-11-21 18:42:06 +0200

    implemented JPA Visit Service, added loader data. Closes #46

--- pet-clinic-data/src/main/java/com/kennshi/petclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/com/kennshi/petclinic/services/map/VisitMapService.java
@@ -2,9 +2,10 @@ package com.kennshi.petclinic.services.map;
 
 import com.kennshi.petclinic.model.Visit;
 import com.kennshi.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
-
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
     @Override
     public Set<Visit> findAll() {
